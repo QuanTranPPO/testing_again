f2bcd6c2a76a4e6ba277cc5b115df805977fa58e
quantran <74757965+QuanTranPPO@users.noreply.github.com>
2020-11-20 16:58:11 -0600

    Add "to see the difference" to the last sentence
    This is optional extended comment for the commit changes

--- print_something.R
+++ print_something.R
@@ -2,4 +2,4 @@ print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
 
-#Try to give some change to the file
+#Try to give some change to the file to see the difference
